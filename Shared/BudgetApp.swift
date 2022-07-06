2cef8e3d6f26b8070a33788d7485caf7d652f56b
hohey670@gmail.com <niloud@liaoqinglins-MacBook-Pro.local>
2022-07-06 22:43:07 +0800

    first commit

---
+++ Shared/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Shared
+//
+//  Created by 廖慶麟 on 2022/7/6.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
